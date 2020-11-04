5c295e736f35f8f7763505d19729ebdc13454ab2
dr497 <anand.ratiney@gmail.com>
2020-11-04 13:32:23 +0800

    Create them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Suspense } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import {
-  ThemeProvider,
-  unstable_createMuiStrictModeTheme as createMuiTheme,
-} from '@material-ui/core/styles';
-import blue from '@material-ui/core/colors/blue';
+import { ThemeProvider } from '@material-ui/core/styles';
 import NavigationFrame from './components/NavigationFrame';
 import { ConnectionProvider } from './utils/connection';
 import WalletPage from './pages/WalletPage';
@@ -14,20 +9,9 @@ import LoadingIndicator from './components/LoadingIndicator';
 import { SnackbarProvider } from 'notistack';
 import PopupPage from './pages/PopupPage';
 import LoginPage from './pages/LoginPage';
-
+import theme from './theme';
 export default function App() {
   // TODO: add toggle for dark mode
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-          primary: blue,
-        },
-      }),
-    [prefersDarkMode],
-  );
 
   // Disallow rendering inside an iframe to prevent clickjacking.
   if (window.self !== window.top) {
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    main: 'rgb(19, 24, 47)',
+    background: {
+      default: 'white',
+    },
+  },
+  //
+  overrides: {},
+});
+
+export default theme;
