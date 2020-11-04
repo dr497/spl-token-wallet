7daf5e70f3c6c498b6fa0a6588f16b828ccac6de
dr497 <anand.ratiney@gmail.com>
2020-11-04 15:28:58 +0800

    More customization

--- src/components/SeedPhrase.js
+++ src/components/SeedPhrase.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useLocalStorageState } from '../utils/utils';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -8,14 +7,7 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import Dialog from '@material-ui/core/Dialog';
 import TextField from '@material-ui/core/TextField';
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    marginLeft: theme.spacing(1),
-  },
-}));
-
 const ShowSeedButton = () => {
-  // const classes = useStyles();
   const [seedWords] = useLocalStorageState('unlocked', '');
   const [open, setOpen] = useState(false);
   if (!seedWords) {
@@ -43,19 +35,6 @@ const ShowSeedButton = () => {
               value={seedWords?.mnemonic}
               label="Seed Words"
               onFocus={(e) => e.currentTarget.select()}
-              // InputLabelProps={{
-              //   classes: {
-              //     root: classes.cssLabel,
-              //     focused: classes.cssFocused,
-              //   },
-              // }}
-              // InputProps={{
-              //   classes: {
-              //     root: classes.cssOutlinedInput,
-              //     focused: classes.cssFocused,
-              //     notchedOutline: classes.notchedOutline,
-              //   },
-              // }}
             />
           </DialogContentText>
         </DialogContent>
